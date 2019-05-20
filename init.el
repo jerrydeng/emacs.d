af1371ee91a250c2f1f6a4c448aa5200c68e1b89
Jerry Deng <jerry.deng@digitalscientists.com>
2019-05-20 15:45:34 -0400

    Allow Copy and Paste between Emacs and Mac Clipbard

--- init.el
+++ init.el
@@ -177,6 +177,21 @@
 (require 'init-local nil t)
 
 
+;;----------------------------------------------------------------------------
+;; Allow users to copy and paste between Emacs and Mac
+;;----------------------------------------------------------------------------
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
+
 
 (provide 'init)
 
